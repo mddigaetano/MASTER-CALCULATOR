c1a79ba93f230e7c510e01ce17b5503a92b30bf0
Amedeo Di Gaetano <amegro.agaty@gmail.com>
2015-02-15 12:47:36 +0100

    Creati header.h e functions.cpp(da rinominare); codice non modificato.

---
+++ header.h
@@ -0,0 +1,37 @@
+//Direttive pre-processore
+
+#include <stdlib.h>         //libreria comandi standard
+#include <iostream>         //input/output c++
+#include <math.h>           //funzioni matematiche
+#include <complex>          //funzioni e definizione numeri complessi
+#include <string.h>         //funzioni per le stringhe
+
+using namespace std;        //per non scrivere "std::" sui comandi delle librerie senza .h
+
+struct s_ModuloPhi{         //dichiarazione struttura per numeri complessi espressi "modulo<phi"
+    double modulo;
+    double phi;
+}; typedef s_ModuloPhi MF;  //dichiarazione tipo variabile derivato
+
+//Prototipi Funzioni
+
+int Fattoriale(int a);
+int RadiciPerfette(int radicando, int indice);
+//void ProvaDivisibilita(int a);                      DA FARE
+//void Conversione(int numero, int base);             DA FARE
+//int Scomposizione(int a);                           DA FARE
+//void EquazioniSecondoGrado();                       DA FARE
+//void Sistemi();                                     DA FARE
+//void Sistemi3();                                    DA FARE
+//void TavolaPitagorica();                            DA FARE
+int MCD(int a, int b);
+//int MinimoComuneMultiplo(int a, int b);             DA FARE
+//double FunzioniTrigonometriche(int a);              DA FARE
+//void NumeriComplessi();                             DA FARE
+//void Risonanza(double induttanza, double capacita); DA FARE
+
+//Prototipi Funzioni Extra
+
+void InputDouble(double *af, double *bf);
+bool segno(double a);                               //TESTARE
+void ListaFunzioni();                               //DA FARE (finire con "\n\n")
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
 CND_ARTIFACT_NAME_Debug=master-calculator
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/master-calculator
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/master-calculator
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=master-calculator.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/master-calculator.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/master-calculator.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
